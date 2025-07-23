2406260e06892dbe64c031c85e9986550f3d84d3
xxxMichael <mayckolchavez27@gmail.com>
2025-07-23 17:05:45 -0500

    Se adecua para consumir servicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Header from './components/sections/Header';
 import Hero from './components/sections/Hero';
+import About from './components/sections/About';
+import Projects from './components/sections/Projects';
+import Skills from './components/sections/Skills';
+import Certifications from './components/sections/Certifications';
+// import Experience from './components/sections/Experience';
+// import Contact from './components/sections/Contact';
 import './App.css';
 
 function App() {
@@ -8,6 +14,12 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Certifications />
+      {/* <Experience />
+      <Contact /> */}
     </div>
   );
 }
